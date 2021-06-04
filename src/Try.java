9cc301e1eb00ed626efacec78aa7cef5b0f7d0a3
2jelee <2ttobokk@gmail.com>
2021-06-04 18:09:01 +0900

    2

--- src/Try.java
+++ src/Try.java
@@ -10,7 +10,7 @@ public class Try {
 			System.out.print("나이를 입력하세요:");
 			int intAge = sc.nextInt();//<= 문자를 입력하면 예외발생
 			int ageAfter10 = intAge + 10;
-			System.out.println("당신의 10년후 나이는:"+ ageAfter10);
+			System.out.println("당신의 10년후 나이는 : "+ ageAfter10);
 		}
 		catch(InputMismatchException e) {
 			System.out.println("나이를 문자형태로 입력하면 앙데요");
