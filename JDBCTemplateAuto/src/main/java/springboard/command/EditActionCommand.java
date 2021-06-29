2fd6680bf1c3b3df9bf107f1b995991287fd57d9
2jelee <82863823+2jelee@users.noreply.github.com>
2021-06-29 21:23:06 +0900

    src

---
+++ JDBCTemplateAuto/src/main/java/springboard/command/EditActionCommand.java
@@ -0,0 +1,37 @@
+package springboard.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import springboard.model.JDBCTemplateDAO;
+import springboard.model.SpringBbsDTO;
+
+@Service
+public class EditActionCommand implements BbsCommandImpl {
+
+	@Autowired
+	JDBCTemplateDAO dao;
+	
+	@Override
+	public void execute(Model model) {
+		Map<String, Object> map = model.asMap();
+		
+		//커맨드객체를 통해 모든 폼값을 저장한 DTO를 가져온다.
+		HttpServletRequest req = (HttpServletRequest)map.get("req");
+		SpringBbsDTO springBbsDTO = (SpringBbsDTO)map.get("springBbsDTO");
+		
+//		JDBCTemplateDAO dao = new JDBCTemplateDAO(); // <<이를 사용하므로써 폼값을 개별적으로 받지 않아도 됨. 한번에 받아옴.
+													/* 
+													String idx = req.getParameter("idx");  
+													... (중략)
+													//dao.edit(idx, name, title, contents, pass); << 여기서 이를 대체함.
+													*/
+		
+		dao.edit(springBbsDTO);
+	}
+} 
