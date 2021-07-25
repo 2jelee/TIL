d30e3422fffb6ce99fd4d1b254ad136b57f99bfb
2jelee <82863823+2jelee@users.noreply.github.com>
2021-07-25 11:42:10 +0900

    src 210725

--- A15Intro/app/src/main/java/com/kosmo/a15intro/Intro.java
+++ A15Intro/app/src/main/java/com/kosmo/a15intro/Intro.java
@@ -1,14 +1,13 @@
 package com.kosmo.a15intro;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.logging.Handler;
+import android.os.Bundle;
 
 /*
-Manifest.xml 파일을 수정하여 Intro 액티비티가 가장 먼저 실행되도록 처리함.
-*/
+Manifest.xml파일을 수정하여 Intro액티비티가 가장 먼저 실행되도록 처리한다.
+ */
 public class Intro extends AppCompatActivity {
 
     //일정시간 이후에 실행하기 위해 Handler 객체를 생성한다.
@@ -22,36 +21,32 @@ public class Intro extends AppCompatActivity {
             //액티비티 실행
             startActivity(intent);
             /*
-            액티비티가 실행되거나 종료될 때 애니메이션 효과를 부여.
+            액티비티가 실행되거나 종료될때 애니메이션 효과를 부여한다.
             인자1 : 새롭게 실행되는 액티비티의 애니메이션
             인자2 : 종료되는 액티비티에 적용되는 애니메이션
              */
-            overridePendingTransition(R.anim.slide_in_right, R.anim.hold);
-            //인트로 액티비티를 종료함.
+            overridePendingTransition(R.anim.slide_in_left, R.anim.hold);
+            //인트로 액티비티를 종료한다.
             finish();
         }
     };
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
     }
-
-    //액티비티 실행 시 3번째로 호출되는 수명주기 메소드
+    //액티비티 실행시 3번째로 호출되는 수명주기 메소드
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Intro 액티비티에 진입한 후 2초 후에 runnable 객체를 실행
+        //Intro액티비티에 진입한 후 2초후에 runnable 객체를 실행한다.
         handler.postDelayed(runnable, 2000);
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-
-        // Intro가 종료될 때 handler에 예약해놓은 작업을 취소함.
+        //Intro가 종료될때 handler에 예약해놓은 작업을 취소한다.
         handler.removeCallbacks(runnable);
     }
 }
