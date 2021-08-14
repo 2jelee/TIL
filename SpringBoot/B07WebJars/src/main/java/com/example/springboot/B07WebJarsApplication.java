12ea2095ce98037572d52b9938594bc49862b657
2jelee <82863823+2jelee@users.noreply.github.com>
2021-08-14 20:05:51 +0900

    WebJars

---
+++ SpringBoot/B07WebJars/src/main/java/com/example/springboot/B07WebJarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B07WebJarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B07WebJarsApplication.class, args);
+	}
+
+}
