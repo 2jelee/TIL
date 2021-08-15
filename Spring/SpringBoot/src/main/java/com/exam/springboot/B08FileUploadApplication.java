667785b995ccb69b0eb500464df0ef8648e388ab
2jelee <82863823+2jelee@users.noreply.github.com>
2021-08-15 16:09:31 +0900

    B08FileUpload

---
+++ Spring/SpringBoot/src/main/java/com/exam/springboot/B08FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.exam.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B08FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B08FileUploadApplication.class, args);
+	}
+
+}
