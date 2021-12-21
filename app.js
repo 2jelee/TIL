52d107770a3e53278cc9862233c06665f0737442
2jelee <2ttobokk@gmail.com>
2021-12-21 23:53:39 +0900

    Events fin.

--- app.js
+++ app.js
@@ -1,61 +1,47 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+// 추가
+const link = document.querySelector("a");
 
-// function onLoginBtnClick(){ 
-//   const username = loginInput.value;
-//   console.log(username);
-// }
-
-/*
-form의 submit event를 감지하고 있는데 
-submit은 엔터를 누르거나 버튼을 클릭할 때 발생한다는 사실을 기억
-*/
-/*
-값을 입력하고 enter함으로써 새로고침이 발생하는 것은 form submit의 '기본(default) 동작' (from 브라우저)
-*/
-// function onLoginSubmit2(){ 
-//   const username = loginInput.value;
-//   console.log(username);
-// }
-
-// loginForm.addEventListener("submit", onLoginSubmit);
-// loginForm.addEventListener("submit", onLoginSubmit()); //우리는 onLoginSubmit에 ()를 추가하지 않았다.
-// 이유? ()를 추가한다는 것은 function을 '즉시' 실행한다는 뜻인데, 우린 바로 실행되는 것을 원치않기 때문! >> submit event가 발생하면 브라우저가 알아서 해당 fucntion을 실행시켜줄 것 ★★★
-
-// onLoginSubmit() // () << 이것을 더한다면 브라우저가 보자마자 자동으로 이 fucntion을 실행시킴
-
-// 즉, 현재 상태에서는 addEventListener를 활용할 때는 바로 실행시키려고 하는 것이 X (지정한 event가 발생했을 때만 브라우저가 해당 func을 실행)
-/*
-★Point 
-: event가 발생할 때 브라우저가 해당 function을 호출하게 되는데 () << 이처럼 빈 상태로 호출하는 것이 아니라,
-() 안에 첫 번째 argument로써 추가적인 정보를 가진 채로 호출하게 된다.
-*/
-
-// function onLoginSubmit(tomato){ 
-//   tomato.preventDefault(); 
-//   console.log(tomato);
-// }
-// loginForm.addEventListener("submit", onLoginSubmit);
-
-/*
-해당 코드를 실행하게 될 경우,
-onLoginSubmit function에 대한 argument로 무언가 정보를 얻게 된다는 것을 browser console에서 확인 가능.
-
-해당 코드는 onLoginSubmit이라는 func이 하나의 argument(=tomato)를 받도록 하는 코드.
-이 func이 하나의 argument를 받도록 하고 그것을 JS에 넘겨주고 있다.
-누군가 form을 submit하면 JS가 이 func을 호출하도록 함.
->> onLoginSubmit({information})
-
-★★★ 모든 EventListener function의 첫 번째 argument는 항상 지금 막 벌어진 일들에 대한 정보가 된다.
-방금 일어난 event에 대한 정보를 지닌 argument를 채워넣게 될 것.
-값 입력 후 browser의 console에서 여러 정보가 표시됨을 확인할 수 있으며 이 정보들은 방금 실행된 event이다.
-*/
-
-// preventDefault라는 function : 어떤 event의 기본 행동이든지 발생되지 않도록 막는다.
-
-// [관행] tomato → event      >> event object를 argument로 주는
 function onLoginSubmit(event){ 
-event.preventDefault(); 
-console.log(loginInput.value); // 버튼을 클릭해도 새로고침이 안되도록
+  event.preventDefault(); 
+  console.log(loginInput.value); 
 }
 loginForm.addEventListener("submit", onLoginSubmit);
+
+// 추가
+// function handleLinkClick(){
+//   alert("clicked!"); //지금 이 alert가 이 page가 다른 동작을 하지 못하도록 막고 있는 상태
+// }
+// link.addEventListener("click", handleLinkClick); // handleLinkClick()로 코드를 작성하게 된다면, 이 함수는 한 번만 실행되고 그것으로 END
+//즉, 코드 작성 시 개발자는 JS한테 함수 이름만 주고 실행하는 것은 JS의 몫이다.
+
+/*
+무엇이 클릭되었는지, 어디가 클릭되었는지 등 정보를 알고 싶다면,
+JS는 단순히 함수를 실행시키기만 하는 것이 아니라
+JS는 함수를 실행시키는 동시에 그 함수에 첫 번째 인자로 object를 넣어줄 것임.
+>> handleLinkClick({information about the event that just happened!!})
+또한, 이 object에는 방금 일어난 event에 대한 여러 정보가 담겨있다.
+방금 일어난 event에 대한 정보를 담은 이 object는 handleLinkClick을 위한 EventListener 함수의 첫 번째 인자로 주어지게 될 것임.
+개발자는 이와 같이 자리를 제공해주면 되고,
+                   공간만 만들고 받기만 하면 된다.
+                   해야할 것은 받는 것뿐임.
+*/
+
+// 추가 + argument + preventDefault()
+function handleLinkClick(event){
+  event.preventDefault(); //이로써 event는 멈추게 되고 아무 것도 진행되지 않는다 >> 즉, 브라우저에 대한 것을 개발자가 control
+  console.dir(event); 
+}
+link.addEventListener("click", handleLinkClick); 
+/*
+[실행 결과]
+Edge 브라우저 : PointerEvent
+Chrome : MouseEvent >> 단, MouseEvent보다 PointerEvent가 더 많은 기능을 포함하고 있어서 현재 PointerEvent를 쓴다고 한다. (2021.12.21 기준)
+*/
+
+/*
+★ 정리 -중요 ★
+addEventListener 안에 있는 함수는 직접 실행하지 않는다는 것.    >> 개발자가 하는 것이 아니라 브라우저가 해주는 것이다!
+                                                                >> 또한, 브라우저는 실행만 시켜주는 것이 아니라 event에 대한 정보도 담아줌.
+*/
