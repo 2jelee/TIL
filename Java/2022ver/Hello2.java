22507aad5fe0e3732768ec6090032126a5d84057
2jelee <2ttobokk@gmail.com>
2022-01-25 22:23:18 +0900

    JAVA basic - basic, dataType, compile(r)

--- Java/2022ver/Hello2.java
+++ Java/2022ver/Hello2.java
@@ -3,7 +3,7 @@ class Hello {
 	
 		System.out.print("Hello World!"); //문장종결기호 (세미콜론)
 		System.out.println("\n한글"); //한글도 괜찮음
-		System.out.printf("%c %d %s",'A', 100, "KOSA");
+		System.out.printf("%c %d %s",'A', 100, "STUDY");
 	} //end main
 } // end class
 
--- Java/2022ver/Info.java
+++ Java/2022ver/Info.java
@@ -1,6 +1,6 @@
 class Info {
 	public static void main(String[] args){ //시작점(진입점)
-		System.out.printf("%s %s %s","이지은", "010-1234-5678", "인천광역시");
+		System.out.printf("%s %s %s","2jelee", "010-1234-5678", "인천광역시");
 	} //end main
 } // end class
 
