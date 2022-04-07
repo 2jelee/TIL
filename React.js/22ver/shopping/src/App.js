091800cb71b5e2292783be2ecb4b41e19825dfb8
2jelee <2ttobokk@gmail.com>
2022-04-07 17:42:22 +0900

    React.js - shopping

--- React.js/22ver/shopping/src/App.js
+++ React.js/22ver/shopping/src/App.js
@@ -1,83 +1,94 @@
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
-import './App.css';
+/* eslint-disable */
 
-import Data from './data'; // Data라는 이름으로 쓰겠다.
-import Card from './Card';
-import { useState } from 'react';
+import { useState } from "react";
+import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import "./App.css";
+import Data from "./data.js";
+import Card from "./Card";
 
-// Router 사용하기
-// npm i react-router-dom@5.2.0
-// BrowserRouter : 서버까지 다녀옴 / HashRouter : 서버까지는 X
-import { Link, Route, Switch } from 'react-router-dom'; 
-import { Detail01, Detail02, Detail03 } from './Detail'; 
-//라이브러리를 가져오므로 form ' ~ ' <-> 경로로 가져오는 것들은 라이브러리 X
+import { Link, Route, Switch } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+//1.
+// export let InventContext = React.createContext();
 
 function App(e) {
-  //변경되어 사용되길 바란다 => useState
-  let[shirt, setShirt] = useState(Data); //Data => data.js
+  let [shrit, setShrit] = useState(Data);
+
+  // 2. 재고, 전달할 데이터
+  // let [invent, setInvent] = useState([9, 10, 11]);
 
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-          <Navbar.Brand><Link to="/">여성 의류 쇼핑몰</Link></Navbar.Brand>
+      <div className="container">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand to="/">여성의류 쇼핑몰</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/detail">상세페이지</Nav.Link>
-              <NavDropdown title="Drop메뉴" id="basic-nav-dropdown">
-                <NavDropdown.Item><Link to="/cart">abc</Link></NavDropdown.Item>
-                <NavDropdown.Item><Link to="/cart">abc</Link></NavDropdown.Item>
-                <NavDropdown.Item><Link to="/cart">abc</Link></NavDropdown.Item>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/detail/0">
+                상세페이지
+              </Nav.Link>
+              <NavDropdown title="드롭메뉴" id="basic-nav-dropdown">
+                <NavDropdown.Item>
+                  <Link to="/cart">Cart</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item><Link to="/cart">abc</Link></NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
-      </Navbar>
-      
-      {/* <Switch> */}
-        {/* <Route exact path="/"> */}
-        <Route exact path="/">
-          <div class="jumbotron">
-            <p className='jp'>
-              <Button variant="primary">Learn more</Button>
-            </p>
-          </div>
+        </Navbar>
 
-          <button onClick={ (e) => {
-            console.log(e.target);
-          }}>event</button>
-
-          <div className="container">
-            <div className="row">
-              {
-                shirt.map( (a, i) => {
-                  return <Card shirt={shirt[i]} i={i} key={i} />
-                })
-              }
-              <Card shirt={shirt} /> 
+        <Switch>
+          <Route exact path="/">
+            <div className="jumbotron">
+              <p className="jp">
+                <Button variant="primary">Learn more</Button>
+              </p>
             </div>
-          </div> 
-        </Route>
 
-        <Route path="/detail/01">
-          <Detail01 /> 
-        </Route>
-        <Route path="/detail/02">
-          <Detail02 /> 
-        </Route>
-        <Route path="/detail/03">
-          <Detail03 /> 
-        </Route>
+            {/* <div>
+      <Change />
+     </div> */}
 
-        <Route path="/cart">
-          <div>장바구니</div>
-        </Route>
-      {/* </Switch> */}
+            <div className="container">
+              <div className="row">
+                {/* 3. 전달할 컴포넌트 감싸다 */}
+                {/* <InventContext.Provider value={invent} > */}
+                  <div className="col">
+                    {shrit.map((a, i) => {
+                      return <Card shrit={shrit[i]} i={i} key={i} />;
+                    })}
+                  </div>
+                {/* </InventContext.Provider> */}
+              </div>
+            </div>
+          </Route>
+
+          <Route path="/detail/:id">
+            <Detail shrit={shrit} />
+          </Route>
+
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
-
 
 export default App;
