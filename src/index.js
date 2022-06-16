b1ff530b6d6b0fa040991a9dbb0f1b3b531e1e24
2jelee <82863823+2jelee@users.noreply.github.com>
2022-06-16 22:56:09 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,27 +6,24 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import axios from "axios";
-import store from "./store";
-// import {persistStore} from "redux-persist";
-// import {PersistGate} from "redux-persist/integration/react";
-// import store from "./store";
+import store from "./store/configure";
+import {CookiesProvider} from "react-cookie";
+
+import {PersistGate} from "redux-persist/integration/react";
+// import { persistor, store } from './redux/store';
 
 axios.defaults.withCredentials = true;
 
-// let persistor = persistStore(store);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <Provider store={store}>
-    {/* store.js에 있는 state를 App이 전부 사용 가능 */}
-    {/*    <PersistGate loading={null} persistor={persistor}>*/}
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        {/*</PersistGate>*/}
-    </Provider>
-  // </React.StrictMode>
+    <CookiesProvider>
+        <Provider store={store}>
+            {/*<PersistGate loading={null} persistor={persistor}>*/}
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            {/*</PersistGate>*/}
+        </Provider>
+    </CookiesProvider>
 );
-
 reportWebVitals();
