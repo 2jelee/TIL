e5f92b09940cbe209a6cde51b1b5e785306796dd
2jelee <82863823+2jelee@users.noreply.github.com>
2022-06-16 22:52:22 +0900

    Create Main.js

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Employee from "./Employee";
+
+function Main() {
+  return (
+      <div>
+        <Layout />
+        {/*<Employee/>*/}
+      </div>
+  );
+}
+
+export default Main;
